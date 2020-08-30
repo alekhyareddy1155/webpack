e9daf89c4d5c9c2c52190531a4477ab5965915e9
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-30 16:04:02 +0530

    handling css

--- tut1/src/index.js
+++ tut1/src/index.js
@@ -1,5 +1,3 @@
-import helloWorld from './hello-world.js';
-import addImage from './add-image.js';
-
-helloWorld();
-addImage();
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
