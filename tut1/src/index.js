44d96637386b754022c253796d8f64d87edf0c08
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-31 11:47:39 +0530

    mode options

--- tut1/src/index.js
+++ tut1/src/index.js
@@ -5,3 +5,11 @@ const heading = new Heading();
 heading.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+if(process.env.NODE_ENV === 'production'){
+    console.log('production mode');
+}else if(process.env.NODE_ENV === 'development'){
+    console.log('development mode');
+}
+
+HelloWorldButton.method1();
