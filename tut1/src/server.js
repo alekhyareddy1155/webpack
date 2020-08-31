bfaf9ff6f4c48de29a49c2605909b0e7cff59f87
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-31 22:25:51 +0530

    getting code for multipage applications

--- tut1/src/server.js
+++ tut1/src/server.js
@@ -1,21 +1,16 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const path = require('path');
 const fs = require('fs');
 
-app.get('/hello-world/',function(req,res){
-    const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
-    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
-    res.send(contentFromHtmlFile);
-});
-app.get('/kiwi/',function(req,res){
-    const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
+app.get('/', function (req, res) {
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
     const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
     res.send(contentFromHtmlFile);
 });
 
-app.use('/static', express.static(path.resolve(__dirname, '../dist/')));
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('Application is running on http://localhost:3000');
 });
