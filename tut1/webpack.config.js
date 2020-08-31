a8b8b4d3b2a5aad47ef4adea221f9e5d7031e242
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-31 10:55:49 +0530

    generating html files automatically duirng webpack build process

--- tut1/webpack.config.js
+++ tut1/webpack.config.js
@@ -2,13 +2,13 @@ const path=require('path');
 const TerserPlugin=require('terser-webpack-plugin');
 const MiniCssExtractPlugin=require('mini-css-extract-plugin');
 const {CleanWebpackPlugin }=require('clean-webpack-plugin');
-
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
     entry: './src/index.js',
     output: {
         filename:'bundle.[contenthash].js',
         path:path.resolve(__dirname, './dist'),
-        publicPath:'dist/'
+        publicPath:''
     },
     mode:'none',
     module:{
@@ -49,11 +49,7 @@ module.exports={
         new MiniCssExtractPlugin({
             filename:'styles.[contenthash].css'
         }),
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns :[
-                '**/*',
-                path.join(process.cwd(),'build/**/*')
-            ]
-        })
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
     ]
 }
