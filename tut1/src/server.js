e2c2ec0ba518e63b28297b0eea1aabd2c91112de
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-31 22:14:40 +0530

    integrating express.js into a multipage application

--- tut1/src/server.js
+++ tut1/src/server.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const app=express();
-// const path = require('path');
-// const fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 
-app.get('/',function(req,res){
-    res.send("some dummy content");
-    // const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
-    // const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
-    // res.send(contentFromHtmlFile);
+app.get('/hello-world/',function(req,res){
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+});
+app.get('/kiwi/',function(req,res){
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
 });
 
-// app.use('/static', express.static(path.resolve(__dirname, '../dist/')));
+app.use('/static', express.static(path.resolve(__dirname, '../dist/')));
 
 app.listen(3000,function(){
     console.log('Application is running on http://localhost:3000');
