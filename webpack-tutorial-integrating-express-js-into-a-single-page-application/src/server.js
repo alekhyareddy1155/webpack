eb21d5a9fd662a127aeb9be8a5e6692a92e73ee1
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-31 19:08:16 +0530

    created simple backend using nodejs and express.js

---
+++ webpack-tutorial-integrating-express-js-into-a-single-page-application/src/server.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+app.get('/',function(req,res){
+    res.send('some dummy content');
+});
+
+app.listen(3000,function(){
+    console.log('Application is running on http://localhost:3000');
+})
